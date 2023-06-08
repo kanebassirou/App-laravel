60effe239b32fbad505e766468b65f25de2ecedf
kanebassirou <kaneb1927@gmail.com>
2023-06-08 18:38:10 +0000

    remove duplication

---
+++ resources/views/base.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Hello city</title>
+
+     <body>
+     @yield('content')
+        
+    
+        <footer>
+            <p>&copy; {{date('Y')}} &middot; <a href="/abouts-us">About Us</a></p>
+        </footer>
+    </body>
+</html>
+
--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,16 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>About-us | hello city </title>
-
-     <body>
+@extends('base')
+@section('content')
         <p>maison avec &hearts; avec les professeurs du Net.</p>
         <p><a href="/">Revenir à la page d'acceuil</a></p>
-        <footer>
-            <p>&copy; {{date('Y')}} &middot; <a href="#">About Us</a></p>
-        </footer>
-    </body>
-</html>
+        
+   @endsection
